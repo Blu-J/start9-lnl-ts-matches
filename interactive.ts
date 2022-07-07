4b16eb46a9237d5ba77e45dee9be2ec563e2da60
BluJ <dragondef@gmail.com>
2022-07-07 11:41:39 -0600

    chore: error messages for wrong in interactive

--- interactive.ts
+++ interactive.ts
@@ -34,10 +34,11 @@ const configBadMispell ={
     'min_chan_size': 1
 }
 
-console.assert(check(configGood) === undefined)
-console.assert(check(configBadCheck) === `Maximum Channel Size must exceed Minimum Channel Size`)
-console.assert(typeof check(configBadType) === 'string' && check(configBadType) !== `Maximum Channel Size must exceed Minimum Channel Size`)
-console.assert(check(configBadMispell) === `Maximum Channel Size must exceed Minimum Channel Size`)
+console.assert(check(configGood) === undefined, 'Our good case should be undefined')
+console.assert(check(configBadCheck) === `Maximum Channel Size must exceed Minimum Channel Size`, 'Our check should tell us that something is wrong')
+console.assert(typeof check(configBadType) === 'string', 'For the bad type, we should be returning an error')
+console.assert(check(configBadType) !== `Maximum Channel Size must exceed Minimum Channel Size`, 'The error returned from the check shouldn\'t be the same as our error')
+console.assert(check(configBadMispell) === `Maximum Channel Size must exceed Minimum Channel Size`, 'Misspel will count as not exist, and therefore the original error')
 
 
 
